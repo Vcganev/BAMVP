32e74ecab0c188a696c77086855f1f16d69fa6fc
Victor G. <vc.ganev@gmail.com>
2024-05-04 18:41:32 +0200

    Update
    Other Points for orientation, still unsure why

--- main.py
+++ main.py
@@ -2,11 +2,12 @@ import pybullet as p
 from robot_setups import setups
 from simulation import environment
 
+start_orientation = p.getQuaternionFromEuler([0,0,0])
 trajectory = [
 
-    ((0.0, 3.0, 1.0), (0.0, 0.0, 0.0, 1)),
-    ((3.0, 0.0, 1.0), (0.0, 0.0, 0.0, 1)),
-    # more tuples/lists following the same structure
+    ((0.0, 3.0, 1.0), (0.0, 0.0, 0.965925, 0.25881905)),
+    ((3.0, 0.0, 1.0), (0.0, 0.0, 0.5, 0.866)),
+    # more tuples/lists following the same structure 
 ]
 
 
