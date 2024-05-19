93921c19de8586888868959339e64d2aeaf43cfd
Victor G. <vc.ganev@gmail.com>
2024-05-19 17:20:55 +0200

    Update Setups Logic
    Added all the Robots and Logic

--- robot_setups/setups.py
+++ robot_setups/setups.py
@@ -6,34 +6,32 @@ import numpy as np
 
 
 def setup_robot_by_type(robot_type):
-       if robot_type == "Scara Robot":
-              return setup_scaraRobot
-       else:
-              raise ValueError("Unsupported robot type: ", robot_type)
-#threshold = 0.02
-#Setup scaraRobot
-#target_positions = [[0, 3, 1], [3, 0, 1],[-3, 0, 1], [0, -3, 1] ]
+    if robot_type == "Scara Robot":
+        return setup_scaraRobot
+    elif robot_type == "Gantry Robot":
+        return setup_gantryRobot
+    elif robot_type == "Articulated Robot":
+        return setup_articulatedRobot
+    else:
+        raise ValueError("Unsupported robot type: " + robot_type)
+
 
 def setup_scaraRobot():
-    urdf_file = "C:/Users/victo/OneDrive/Dokumente/BA/Matlab/reachabilityExperiment/Ex_1_scaraRobot4/scaraRobot4.urdf"
-    #physics_client = p.connect(p.DIRECT)
-    #p.setRealTimeSimulation(1)
-    #p.setPhysicsEngineParameter(numSolverIterations=1000)
+    urdf_file = "C:/Users/victo/OneDrive/Dokumente/BA/REALMVP/BAMVP/URDF Codes/scaraRobot.urdf"
+
+    start_orientation = p.getQuaternionFromEuler ([0, 0, 0])
+    robot = pi.RobotBase(urdf_file, [0, 0, 0], start_orientation, 'end_effector')
+    return(robot)
+
+def setup_gantryRobot():
+    urdf_file = "C:/Users/victo/OneDrive/Dokumente/BA/REALMVP/BAMVP/URDF Codes/gantryRobot.urdf"
     start_orientation = p.getQuaternionFromEuler ([0, 0, 0])
     robot = pi.RobotBase(urdf_file, [0, 0, 0], start_orientation, 'end_effector')
     return(robot)
 
 
-#robot = setup_scaraRobot()
-#for target_position in target_positions:
-    robot.set_endeffector_pose(target_position)
-    time.sleep(1)
-    current_position, _ = robot.get_endeffector_pose('end_effector')
-    distance = np.linalg.norm(current_position - target_position)
-    if distance <= threshold:
-            print("Point",target_position, "reached")
-    else:
-            print("Point",target_position, "not reached")
-        # Sleep for a bit to visually inspect the simulation
-    time.sleep(3)
-    
+def setup_articulatedRobot():
+    urdf_file = "C:/Users/victo/OneDrive/Dokumente/BA/REALMVP/BAMVP/URDF Codes/articulatedRobot2.urdf"
+    start_orientation = p.getQuaternionFromEuler ([0, 0, 0])
+    robot = pi.RobotBase(urdf_file, [0, 0, 0], start_orientation, 'end_effector')
+    return(robot)
